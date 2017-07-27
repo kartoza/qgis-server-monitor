14ef263f15592cd4ba0adbe13c382111026b9d85
Tim Sutton <tim@kartoza.com>
2017-07-27 21:35:57 +0200

    Initial Commit

---
+++ ServerMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServerMonitor
+//
+//  Created by Tim Sutton on 2017/07/27.
+//  Copyright © 2017 Tim Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
