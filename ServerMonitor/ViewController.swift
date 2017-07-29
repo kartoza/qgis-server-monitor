8e5b32e18eb223033d301b7e7fa0864592520e25
Tim Sutton <tim@kartoza.com>
2017-07-29 15:49:21 +0200

    First version with a button and that pings out to QGIS.org

--- ServerMonitor/ViewController.swift
+++ ServerMonitor/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBAction func refreshPressed(_ sender: Any) {
+        print ("Refresh pressed");
+        let scriptUrl = "http://qgis.org"
+        let myUrl = NSURL(string: scriptUrl)
+        let request = NSMutableURLRequest(url:myUrl! as URL)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshButton.layer.cornerRadius = 4;
     }
 
     override func didReceiveMemoryWarning() {
