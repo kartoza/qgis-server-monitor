818ff02ed23469ff24b35fd6b6914a8171d8b8dc
Tim Sutton <tim@kartoza.com>
2017-07-30 08:13:26 +0200

    Added simple table view and iterator to check the web sites.

--- ServerMonitor/ViewController.swift
+++ ServerMonitor/ViewController.swift
@@ -9,44 +9,89 @@
 import UIKit
 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var refreshButton: UIButton!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Data model: These strings will be the data for the table view cells
+    let urls: [String] = [
+        "http://qgis.org",
+        "http://plugins.qgis.org"
+    ]
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
     
     @IBAction func refreshPressed(_ sender: Any) {
+        
         print ("Refresh pressed");
-        let scriptUrl = "http://qgis.org"
-        let myUrl = NSURL(string: scriptUrl)
-        let request = NSMutableURLRequest(url:myUrl! as URL)
-        request.httpMethod = "GET"
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
-            data, response, error in
+        
+        for url in urls {
             
-            // Check for error
-            if error != nil
-            {
-                print("error=\(String(describing: error))")
-                return
+            let scriptUrl = url
+            let myUrl = NSURL(string: scriptUrl)
+            let request = NSMutableURLRequest(url:myUrl! as URL)
+            request.httpMethod = "GET"
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                // Check for error
+                if error != nil
+                {
+                    print("error=\(String(describing: error))")
+                    
+                    return
+                }
+                
+                // Print out response string
+                let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                print("responseString = \(String(describing: responseString))")
             }
-            
-            // Print out response string
-            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-            print("responseString = \(String(describing: responseString))")
+            task.resume()
         }
-        task.resume()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         refreshButton.layer.cornerRadius = 4;
+        // Set up the table - see https://stackoverflow.com/questions/33234180/uitableview-example-for-swift
+        // Register the table view cell class and its reuse id
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.urls.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.urls[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
 }
 
